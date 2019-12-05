24e2b4a8138f3eb4b5cf6f6f0f1b30c8e5442767
Emmanuel <aadewale123@gmail.com>
2019-12-05 10:54:28 -0500

    day 1

---
+++ day1/day1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+
+
+unsigned int calculateFuel(unsigned int mass){
+    if(mass < 6) {
+        return 0;
+    }
+    unsigned int round = floor(mass / 3) - 2;
+    return round;
+}
+
+unsigned int iterativefuel(unsigned int fuel){
+    printf("Current: %d", fuel);
+    unsigned int ff = calculateFuel(fuel);
+    unsigned int temp = ff;
+    while (ff != 0){
+        /* code */
+        int tt = calculateFuel(ff);
+        temp += tt;
+    }
+
+    return temp;
+}
+
+int main(){
+    FILE *f;
+    FILE *d;
+    char line[256];
+    char lie[111];
+    unsigned int totalsum = 0;
+    unsigned int secondsum = 0;
+
+    f = fopen("input.txt", "r");
+    // int test = floor(136811/3) - 2;
+    // printf("Fif %d\n", test);
+
+    while (fgets(line, sizeof(line), f)) {
+        int num = atoi(line);
+        int firstmass = calculateFuel(num);
+        totalsum += firstmass;
+    }
+
+    printf("Total sum: %d\n", totalsum);
+    fclose(f);
+
+    d = fopen("input.txt", "r");
+    // Part 2
+    int mass;
+     while (fgets(lie, sizeof(lie), d)) {
+         int num = atoi(lie);
+         int fue = (num / 3) - 2;
+         secondsum += fue;
+         mass = fue;
+         while((fue=(mass/3)-2)>0){
+            secondsum += fue;
+            mass = fue;
+        }
+    }
+
+    printf("Total something: %d\n", secondsum);
+}
+
